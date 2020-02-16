3ce87d22b9261b55840cc10aff70cdada8063500
Vít Herain <vit.herain@zonky.cz>
2020-02-16 20:37:33 +0100

    Added another React test

--- moneytou-fe/src/App.test.js
+++ moneytou-fe/src/App.test.js
@@ -2,8 +2,15 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-it('renders side menu and main body', () => {
-  const renderResult = render(<App />);
-  expect(renderResult.container.querySelector('div#side-menu')).toBeInTheDocument();
-  expect(renderResult.container.querySelector('div#screen-content')).toBeInTheDocument();
+describe('<App /> spec', () => {
+  it('renders the component', () => {
+    const { container } = render(<App />);
+    expect(container).toMatchSnapshot();
+  });
+
+  it('renders side menu and main body', () => {
+    const { container } = render(<App />);
+    expect(container.querySelector('div#side-menu')).toBeInTheDocument();
+    expect(container.querySelector('div#screen-content')).toBeInTheDocument();
+  });
 });
