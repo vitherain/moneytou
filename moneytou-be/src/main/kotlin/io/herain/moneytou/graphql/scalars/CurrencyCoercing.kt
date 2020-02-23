dfc1fec10f5a533ad1a41391f82c9b67d2053964
Vít Herain <vit.herain@zonky.cz>
2020-02-23 20:35:15 +0100

    CurrencyCoercing serialize implemented

--- moneytou-be/src/main/kotlin/io/herain/moneytou/graphql/scalars/CurrencyCoercing.kt
+++ moneytou-be/src/main/kotlin/io/herain/moneytou/graphql/scalars/CurrencyCoercing.kt
@@ -1,6 +1,7 @@
 package io.herain.moneytou.graphql.scalars
 
 import graphql.schema.Coercing
+import graphql.schema.CoercingSerializeException
 import io.herain.moneytou.domain.Currency
 
 class CurrencyCoercing: Coercing<Currency, String> {
@@ -14,6 +15,9 @@ class CurrencyCoercing: Coercing<Currency, String> {
     }
 
     override fun serialize(dataFetcherResult: Any?): String {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        if (dataFetcherResult is Currency) {
+            return dataFetcherResult.name;
+        }
+        throw CoercingSerializeException("dataFetcherResult is not of type ${Currency::class.qualifiedName}");
     }
 }
--- moneytou-be/src/test/groovy/io/herain/moneytou/graphql/scalars/CurrencyCoercingSpec.groovy
+++ moneytou-be/src/test/groovy/io/herain/moneytou/graphql/scalars/CurrencyCoercingSpec.groovy
@@ -1,49 +1,33 @@
 package io.herain.moneytou.graphql.scalars
 
+import graphql.schema.CoercingSerializeException
 import io.herain.moneytou.domain.Currency
 import spock.lang.Specification
-import spock.lang.Unroll
 
 class CurrencyCoercingSpec extends Specification {
 
-    @Unroll
     def 'currency is serialized to String'() {
         given:
-            def coercing = new CurrencyCoercing()
+        CurrencyCoercing coercing = new CurrencyCoercing()
         when:
-            def serializedValue = coercing.serialize(Currency.CZK)
+        String result = coercing.serialize(inputCurrency)
         then:
-            serializedValue == "EUR"
-    }
-
-    @Unroll
-    def 'getName should return whatever name is given'() {
-        given: 'create testable instance'
-            Testable testable = new Testable(inputName)
-
-        when:
-            String result = testable.toString()
-
-        then:
-            result == expectedResult
-
+        result == expectedResult
         where:
-            inputName || expectedResult
-            "Serdar"  || "Serdar"
-            "John"    || "John"
-            "doe"     || "doe"
+        inputCurrency || expectedResult
+        Currency.CZK  || "CZK"
+        Currency.EUR  || "EUR"
+        Currency.USD  || "USD"
     }
 
-    class Testable {
-        final String str
-
-        Testable(final String str) {
-            this.str = str
-        }
-
-        @Override
-        String toString() {
-            return str
-        }
+    def 'currency serialization ends with CoercingSerializeException when input is not Currency'() {
+        given:
+        CurrencyCoercing coercing = new CurrencyCoercing()
+        when:
+        coercing.serialize(input)
+        then:
+        thrown(CoercingSerializeException)
+        where:
+        input << [new Object(), "CZK"]
     }
 }
