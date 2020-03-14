eb605b3fe52ff0e0d37ca2452277fefeae3ff514
Vít Herain <vit.herain@zonky.cz>
2020-03-14 18:30:33 +0100

    SQL script for creating of transaction table

--- moneytou-be/tx/src/main/resources/db/migration/V0001_1__init_schema.sql
+++ moneytou-be/tx/src/main/resources/db/migration/V0001_1__init_schema.sql
@@ -1,12 +1,15 @@
+CREATE EXTENSION "uuid-ossp";
 CREATE SCHEMA IF NOT EXISTS moneytou AUTHORIZATION moneytou_dba;
 
-CREATE SEQUENCE moneytou.sq_transaction
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;
-
 CREATE TABLE moneytou.transaction(
-    id UUID NOT NULL DEFAULT nextval('moneytou.sq_transaction')
+    id UUID NOT NULL DEFAULT uuid_generate_v4(),
+    amount NUMERIC(19,2) NOT NULL,
+    currency VARCHAR(20) NOT NULL,
+    date TIMESTAMP WITHOUT TIME ZONE NOT NULL,
+    category_id BIGINT NOT NULL,
+    account_id BIGINT NOT NULL,
+    note TEXT NULL,
+
+    CONSTRAINT transaction_pkey PRIMARY KEY (id),
+    CONSTRAINT chck_currency CHECK (currency IN ('CZK', 'EUR', 'USD'))
 );
