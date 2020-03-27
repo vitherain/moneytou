05630e56a1fc5eb0c9f98aa05c53f240dba04138
Vít Herain <vit.herain@zonky.cz>
2020-03-27 22:22:07 +0100

    Removed unnecessary extended interface

--- moneytou-be/tx/src/main/kotlin/io/herain/moneytou/tx/transaction/repository/TxRepository.kt
+++ moneytou-be/tx/src/main/kotlin/io/herain/moneytou/tx/transaction/repository/TxRepository.kt
@@ -1,8 +1,7 @@
 package io.herain.moneytou.tx.transaction.repository
 
 import io.herain.moneytou.tx.transaction.domain.Tx
-import org.springframework.data.repository.PagingAndSortingRepository
 import org.springframework.data.repository.Repository
 import java.util.UUID
 
-interface TxRepository : Repository<Tx, UUID>, PagingAndSortingRepository<Tx, UUID>, TxPagingOperations
+interface TxRepository : Repository<Tx, UUID>, TxPagingOperations
