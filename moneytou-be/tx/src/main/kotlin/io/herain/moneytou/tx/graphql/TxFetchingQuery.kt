11317c29bc8757cb9ada5fc04e85963bc907c6bd
Vít Herain <vit.herain@zonky.cz>
2020-03-27 22:35:01 +0100

    Renamed Query class

--- moneytou-be/tx/src/main/kotlin/io/herain/moneytou/tx/graphql/TxFetchingQuery.kt
+++ moneytou-be/tx/src/main/kotlin/io/herain/moneytou/tx/graphql/TxFetchingQuery.kt
@@ -6,7 +6,7 @@ import io.herain.moneytou.tx.transaction.repository.TxPagingOperations
 import org.springframework.data.domain.PageRequest
 import org.springframework.data.domain.Sort
 
-class Query(val txPagingOperations: TxPagingOperations) : GraphQLQueryResolver {
+class TxFetchingQuery(val txPagingOperations: TxPagingOperations) : GraphQLQueryResolver {
 
     private val defaultPageRequest: PageRequest = PageRequest.of(
         0,
